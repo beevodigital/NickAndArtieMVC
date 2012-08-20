d824941bf960d45b28a08eb7ed08014754e0ce01
Brian Evans <brian.evans@space150.com>
2012-08-19 21:33:41 -0500

    more code

---
+++ NickAndArtie/Controllers/GearController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NickAndArtie.Controllers
+{
+    public class GearController : Controller
+    {
+        //
+        // GET: /Gear/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
