452eef5272dbef9924219420f640ae167c24a725
Brian Evans <brian.evans@space150.com>
2012-08-19 19:45:34 -0500

    initial commit

---
+++ NickAndArtie/Controllers/FindUsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NickAndArtie.Controllers
+{
+    public class FindUsController : Controller
+    {
+        //
+        // GET: /FindUs/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ NickAndArtie/Controllers/PodcastsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NickAndArtie.Controllers
+{
+    public class PodcastsController : Controller
+    {
+        //
+        // GET: /Podcasts/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
