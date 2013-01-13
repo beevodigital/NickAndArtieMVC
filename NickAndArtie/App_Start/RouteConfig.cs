5aceb71be8f0da44d0be60fa690f2fc5032b87be
Brian Evans <brian.evans@space150.com>
2013-01-13 09:01:21 -0600

    latest

--- NickAndArtie/App_Start/RouteConfig.cs
+++ NickAndArtie/App_Start/RouteConfig.cs
@@ -14,16 +14,60 @@ namespace NickAndArtie
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "Itunes",
-                url: "wp-content/podcast/feed.xml",
-                defaults: new { controller = "Podcasts", action = "Feed", id = UrlParameter.Optional }
-            );
+               name: "Itunes",
+               url: "wp-content/podcast/feed.xml",
+               defaults: new { controller = "Podcasts", action = "Feed", id = UrlParameter.Optional }
+           );
 
             routes.MapRoute(
                 name: "AdminHome",
                 url: "manage",
                 defaults: new { controller = "ManagePosts", action = "Index", id = UrlParameter.Optional }
             );
+
+            routes.MapRoute(
+                name: "ManageLogin",
+                url: "home/login",
+                defaults: new { controller = "Home", action = "Login", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "ManagePhotoReel",
+                url: "ManagePhotoReel/{action}/{id}",
+                defaults: new { controller = "ManagePhotoReel", action = "Index", id = UrlParameter.Optional }
+            );
+            routes.MapRoute(
+                name: "ManagePodcasts",
+                url: "ManagePodcasts/{action}/{id}",
+                defaults: new { controller = "ManagePodcasts", action = "Index", id = UrlParameter.Optional }
+            );
+            routes.MapRoute(
+                name: "ManagePosts",
+                url: "ManagePosts/{action}/{id}",
+                defaults: new { controller = "ManagePosts", action = "Index", id = UrlParameter.Optional }
+            );
+            routes.MapRoute(
+                name: "ManageRoadTrips",
+                url: "ManageRoadTrips/{action}/{id}",
+                defaults: new { controller = "ManageRoadTrips", action = "Index", id = UrlParameter.Optional }
+            );
+            routes.MapRoute(
+                name: "ManageSlideShows",
+                url: "ManageSlideShows/{action}/{id}",
+                defaults: new { controller = "ManageSlideShows", action = "Index", id = UrlParameter.Optional }
+            );
+            routes.MapRoute(
+                name: "ManageYoutubeVideos",
+                url: "ManageYoutubeVideos/{action}/{id}",
+                defaults: new { controller = "ManageYoutubeVideos", action = "Index", id = UrlParameter.Optional }
+            );
+            //routes.MapRoute(
+            //    name: "UnderContruction",
+            //    url: "{*url}",
+            //    defaults: new { controller = "Utility", action = "Index", id = UrlParameter.Optional }
+            //);
+
+           
 
             routes.MapRoute(
                 name: "PostDetail",
